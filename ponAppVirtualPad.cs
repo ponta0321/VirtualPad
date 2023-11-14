91ea330c0a65b3f746d1874f5f4cee5f74ae2aad
ponta0321 <p0ntanext@gmail.com>
2023-11-14 23:44:09 +0900

    202311142344

--- ponAppVirtualPad.cs
+++ ponAppVirtualPad.cs
@@ -1,8 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+/*:
+ * @addondesc Addition of virtual gamepad function
+ * @author ponApp
+ * @help A virtual gamepad is displayed on the screen, and by touching the screen you can perform the same operations as using a gamepad.
+ * 
+ */
+
+/*:ja
+ * @addondesc バーチャルゲームパッド機能の追加
+ * @author ponApp
+ * @help 画面上にバーチャルゲームパッドを表示し、画面タッチでゲームパッドの操作と同程度の操作を実現します。
+ * 
+ */
+
 using UnityEngine;
 
-public class ponAppVirtualPad
+namespace RPGMaker.Codebase.Addon
 {
+    public class ponAppVirtualPad
+    {
+
+    }
 
 }
